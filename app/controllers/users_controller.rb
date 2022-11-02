5f94a56c5aca4be82191e472eff6006216e3218a
ritsuki-cs <game.ricky.x@gmail.com>
2022-11-02 04:56:15 +0000

    fix users_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   before_action :logged_in_user, only: [:index, :edit, :update, :destroy,
-                                        :following, :followers]
+                                        :following, :followers, :show]
   before_action :correct_user,   only: [:edit, :update]
   before_action :admin_user,     only: :destroy
   
@@ -10,8 +10,8 @@ class UsersController < ApplicationController
   
   def show
     @user = User.find(params[:id])
-    @micropost = current_user.microposts.build
     @microposts = @user.microposts.paginate(page: params[:page], per_page: 6)
+    @micropost = current_user.microposts.build
   end
   
   def new
