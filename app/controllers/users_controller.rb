6fb24eb4b7e4e4431ec867e81f809fb13123a9d9
ritsuki-cs <game.ricky.x@gmail.com>
2022-12-01 16:31:12 +0000

    change layout for following list

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -52,14 +52,14 @@ class UsersController < ApplicationController
   def following
     @title = "Following"
     @user  = User.find(params[:id])
-    @users = @user.following.paginate(page: params[:page])
+    @users = @user.following.paginate(page: params[:page], per_page: 8)
     render 'show_follow'
   end
 
   def followers
     @title = "Followers"
     @user  = User.find(params[:id])
-    @users = @user.followers.paginate(page: params[:page])
+    @users = @user.followers.paginate(page: params[:page], per_page: 8)
     render 'show_follow'
   end
 
