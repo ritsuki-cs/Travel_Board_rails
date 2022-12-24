63e6ad3b9bfcf708ab648b207c4d0c55ae977f58
ritsuki-cs <game.ricky.x@gmail.com>
2022-12-24 07:52:19 +0000

    change home content

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,7 +2,7 @@ class StaticPagesController < ApplicationController
   
   def home
     if logged_in?
-      @feed_items = Micropost.paginate(page: params[:page], per_page: 6)
+      @feed_items = Micropost.where(user_id: [current_user.id, *current_user.following_ids]).paginate(page: params[:page], per_page: 6)
     end
   end
 end
